1ca20a46b1bb43195f1d6653b7104b7a352a856e
Mikhail Piankov <pyankovmu@ya.ru>
2019-12-20 00:11:14 -0500

    make change in input reading

--- src/main/java/org/ump/UserJoin.java
+++ src/main/java/org/ump/UserJoin.java
@@ -9,15 +9,17 @@ public class UserJoin {
         // read input
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
+        System.out.println(n + " cases to enter: ");
         List<UserWithEmails> users = new ArrayList<>();
         // create users and email.
         while(n > 0){
+            users.add(new UserWithEmails(sc.nextLine()));
             n--;
-            users.add(new UserWithEmails(sc.next()));
         }
 
         // join them
         UsersJoiner clearer = new UsersJoiner();
+        System.out.println("Output");
 
         // print to output
         clearer.clear(users).forEach(
